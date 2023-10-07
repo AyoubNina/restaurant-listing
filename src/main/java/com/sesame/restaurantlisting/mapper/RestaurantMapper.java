73c54a573a8ecc4706e63e03928b4ee6af84c9d5
AyoubNina <jihene.ayoub@gmail.com>
2023-10-07 11:05:37 +0100

    last commit

---
+++ src/main/java/com/sesame/restaurantlisting/mapper/RestaurantMapper.java
@@ -0,0 +1,18 @@
+package com.sesame.restaurantlisting.mapper;
+
+
+import com.sesame.restaurantlisting.dto.RestaurantDTO;
+import com.sesame.restaurantlisting.entity.Restaurant;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+@Mapper
+public interface RestaurantMapper {
+
+    RestaurantMapper INSTANCE = Mappers.getMapper(RestaurantMapper.class);
+
+    Restaurant mapRestaurantDTOToRestaurant(RestaurantDTO restaurantDTO);
+
+    RestaurantDTO mapRestaurantToRestaurantDTO(Restaurant restaurant);
+
+}
